cc2222d8f111ca8bf7259a195c80d7aef7f3aee6
Julio Sandino <sandinojulio98@gmail.com>
2023-04-08 22:28:27 -0700

    feat: adding static file serving to project server

--- atlas_app/atlas_app/urls.py
+++ atlas_app/atlas_app/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home")
 ]
+
+# setting up serve up static file serving
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
